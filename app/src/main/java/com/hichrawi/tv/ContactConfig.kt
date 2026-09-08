5b61febe566e8185d1962f96d7b46b62d8d277b8
hichrawi <hichrawi86@gmail.com>
2026-09-08 10:42:18 +0100

    Add files via upload

---
+++ app/src/main/java/com/hichrawi/tv/ContactConfig.kt
@@ -0,0 +1,8 @@
+package com.hichrawi.tv
+
+/** Static public links for the HICHRAWI LIVE app. */
+object ContactConfig {
+    const val FACEBOOK = "https://www.facebook.com/hichrawibouazizi.officiel"
+    const val WEBSITE = "https://hichrawi-tv-app.web.app/"
+    const val UPDATES = "https://hichrawi-tv-app.web.app/"
+}
