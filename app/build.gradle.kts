b0cae2a550797d0cd896506180a6392a20bd10cf
Hichrawi <hichrawi86@gmail.com>
2026-09-12 21:13:19 +0000

    Build APKs per device architecture

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,15 @@ android {
     }
 
     buildFeatures { buildConfig = true }
+
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+            isUniversalApk = false
+        }
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
