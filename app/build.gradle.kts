ab2baf798f82f0fed2a47a3bf653727a7793cabc
Hichrawi <hichrawi86@gmail.com>
2026-09-17 18:12:56 +0000

    Finalize compact ARMv7 ORCA compatible build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,12 +11,12 @@ android {
     defaultConfig {
         applicationId = "com.hichrawi.tv"
         minSdk = 23
-        targetSdk = 35
-        versionCode = 15
-        versionName = "2.4.0"
+        targetSdk = 27
+        versionCode = 16
+        versionName = "2.5.0"
 
         ndk {
-            abiFilters += listOf("armeabi-v7a", "arm64-v8a")
+            abiFilters += listOf("armeabi-v7a")
         }
     }
 
